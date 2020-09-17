fcff55206ba1ead76948f1b69b675eda1d8c82a0
kumarj <jitendra.kumar@viacomcontractor.com>
2020-09-17 17:35:53 +0530

    code changes for CustomRowMapper through jdbc

--- src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -25,7 +25,7 @@ public class DatabaseDemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("All users -> {}",dao.findAll());
+		logger.info("All users -> {}",dao.findAllCustomRowMapper());
 		logger.info("user id 1001 -> {}",dao.findById(1001));
 		logger.info("deleting 1002 -> No of row deleted- {}",dao.deleteById(1002));
 		logger.info("Inserting 1004-> {}", dao.insert(new Person(1004,"Dola","Khagaria",new Date())));
--- src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDao.java
+++ src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -4,8 +4,11 @@ import com.in28minutes.database.databasedemo.jdbc.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -14,8 +17,26 @@ public class PersonJdbcDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    class PersonRowMapper implements RowMapper<Person>{
+
+        @Override
+        public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+            Person person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setLocation(resultSet.getString("location"));
+            person.setBirthDate(resultSet.getTimestamp("birth_date"));
+            return person;
+        }
+    }
+
     public List<Person> findAll(){
        return jdbcTemplate.query("select * from person",new BeanPropertyRowMapper<Person>(Person.class));
+
+    }
+
+    public List<Person> findAllCustomRowMapper(){
+        return jdbcTemplate.query("select * from person",new PersonRowMapper());
 
     }
 
